ce95cc381e742c0ba6972ea0399e33a4edc01988
yuidegm <misganewendeg879@gmail.com>
2019-12-24 13:46:13 +0300

    payments entity commit

---
+++ entity/payment_entity.go
@@ -0,0 +1,14 @@
+package entity
+
+
+
+type Payment struct {
+	Room_Type               string
+	Number_of_Room          int
+	Number_of_Person        int
+	Number_of_Children      int
+	Checkin_date            string
+	Checkout_date           string
+	Suggestion              string
+
+}
