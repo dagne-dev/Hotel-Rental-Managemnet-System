b0ffe0185d4d376bd4803bc9594c9b67375668e2
yuidegm <misganewendeg879@gmail.com>
2020-01-05 00:35:50 +0300

    repository commit

--- payments/repository.go
+++ payments/repository.go
@@ -6,6 +6,10 @@ type PaymentRepository interface {
 	PaymentReservation(pay entity.Payment)  error
 	IncreamentRate(rate entity.Rating)  error
 	RetrieveHotelRateValue() (entity.Rating,error)
+        GetUserId(updaterate *entity.Rating) ([]entity.Rating,[]error)
+	GetUserRateValue(user_id uint) (*entity.Rating,[]error)
+	UpdateUserRateValue(user_id uint,u int) ([]error)
+
 	//RetrieveAccountFromBank(v int) (entity.Balance,error)
 
 
